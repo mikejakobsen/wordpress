a5e085c05415e9496650c5efd3976a11e3fbe779
Mike Jakobsen <mike@jakobsen.dk>
2017-02-03 11:30:59 +0100

    :bug: Reverting the post-type to single array

--- web/app/themes/frisor/templates/partials/content-single-medarbejdere.blade.php
+++ web/app/themes/frisor/templates/partials/content-single-medarbejdere.blade.php
@@ -5,12 +5,12 @@
     if( $post_objects ): ?>
     <div class="employees">
       @foreach( $post_objects as $post_object)
-        <?php $name = get_field('employee__name', $post_object->ID); ?>
+        <?php setup_postdata( $post_object ); ?>
         <?php $job = get_field('employee__job', $post_object->ID); ?>
-        <?php $image = get_field('employee__photo', $post_object->ID); ?>
+        <?php $image = get_field('employeepost__photo', $post_object->ID); ?>
+          <p><?php the_title(); ?></p>
       <div class="employee__row row-eq-height">
         <div class="employee__item col-xs-12 col-md-4 col-lg-3">
-          <h3 class="employee__name text-center">{{ $name }}</h3>
           <h3 class="employee__job text-center">{{ $job }}</h3>
           <img class="employee__image img-reponsive" src="{{ $image['url'] }}"
           alt="{{ $image['alt'] }}" title="{{ $image['title'] }}"/>
